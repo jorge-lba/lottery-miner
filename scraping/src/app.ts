c36e97642e41a6028828210d0f4996d108eed4f8
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2020-08-02 03:06:55 -0300

    Add - Funçao para retornar valores da caixa de seleçao e funçao sleep

--- scraping/src/app.ts
+++ scraping/src/app.ts
@@ -1,6 +1,25 @@
 import { launch } from 'puppeteer'
 
+async function valuesDropbox(page:any, selector: string){
+    const result = await page.$$eval(
+        selector + ' option', 
+        all => all.map((element:any) => element.value)
+    )
+
+    return result
+}
+
+function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+      currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+  }
+
 async function webScraping(url:string, path?:string){
+    const box = '#ctl00_ctl58_g_7fcd6a4b_5583_4b25_b2c4_004b6fef4036_ddlCidade'
+
     const browser = await launch({
         args: [
             '--no-sandbox',
@@ -9,7 +28,13 @@ async function webScraping(url:string, path?:string){
     })
     const page = await browser.newPage()
     await page.goto(url)
+    await page.select('#ctl00_ctl58_g_7fcd6a4b_5583_4b25_b2c4_004b6fef4036_ddlTipo','2')
+    await sleep(1000)
+    await page.select('#ctl00_ctl58_g_7fcd6a4b_5583_4b25_b2c4_004b6fef4036_ddlUf', 'SP')
+    await sleep(1000)
     await page.screenshot({path})
+    const elements = await valuesDropbox(page, box)
+    console.log(elements)
 
     await browser.close()
 }
